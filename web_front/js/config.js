90845a08ac4c0390aade6d3ab6b864d62db0f054
fanyoufd <fanyoufd@outbooks.com>
2020-02-06 13:14:43 +0800

    最新资讯模块完成

---
+++ web_front/js/config.js
@@ -0,0 +1,19 @@
+
+
+
+//  配置文件 
+//  把项目中 所有的 基地址 和 接口 集中在一起进行管理
+//  所有项目的 基地址
+var BASEURL = "http://192.168.137.1:8000"
+
+//  定义对象用来管理接口 
+var APILIST = {
+    // 渲染 文章类别
+    user_load_get: BASEURL + "/category",
+    // 文章搜索 焦点图模块
+    user_search_get: BASEURL + "/search",
+    // 获取热门文章排行
+    user_rank_get: BASEURL + "/rank",
+    // 最新文章
+    user_lastest_get: BASEURL + "/lastest"
+};
---
+++ web_front/js/usil.js
@@ -0,0 +1,33 @@
+
+
+
+//  文章列表模块
+var tabulation = {
+    //  文章类别
+    category: function () {
+        return $.get(APILIST.user_load_get)
+    },
+    //  文章搜索 焦点图模块
+    search: function () {
+        return $.get(APILIST.user_search_get, { perpage: 5 })
+    },
+    //  获取热门文章排行
+    rank: function () {
+        return $.get(APILIST.user_rank_get)
+    },
+    // 最新文章
+    lastest: function () {
+        return $.get(APILIST.user_lastest_get)
+    }
+
+}
+
+
+
+
+
+/*  url: APILIST.user_log_get,
+            type: "post",
+            data: fd,
+            processData: false, // 不允许处理数据
+            contentType: false, // 不要设置请求头 */
