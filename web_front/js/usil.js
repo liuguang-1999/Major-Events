eb103fae512d2915b2130bd75d30b1ab5d462f0e
fanyoufd <fanyoufd@outbooks.com>
2020-02-08 19:19:00 +0800

    ES6方法升级代码 项目完结

--- web_front/js/usil.js
+++ web_front/js/usil.js
@@ -2,43 +2,26 @@
 
 
 //  文章列表模块
-var tabulation = {
+const tabulation = {
     //  文章类别
-    category: function () {
-        return $.get(APILIST.user_load_get)
-    },
+    category: () => $.get(APILIST.user_load_get),
+
     //  文章搜索 焦点图模块
-    search: function () {
-        return $.get(APILIST.user_search_get, { perpage: 5 })
-    },
+    search: () => $.get(APILIST.user_search_get, { perpage: 5 }),
+
     //  获取热门文章排行
-    rank: function () {
-        return $.get(APILIST.user_rank_get)
-    },
+    rank: () => $.get(APILIST.user_rank_get),
+
     // 最新文章
-    lastest: function () {
-        return $.get(APILIST.user_lastest_get)
-    },
+    lastest: () => $.get(APILIST.user_lastest_get),
+
     //  列表页 获取列表数据
-    /**
-     * 类型编号type 
-     * 页码page
-     * 
-     */
-    upto: function (type, page) {
-        return $.get(APILIST.user_search_get, { "type": type, "page": page })
-    },
-    article: function (id) {
-        return $.get(APILIST.user_article_get, { 'id': id })
-    }
+    upto: (type, page) => $.get(APILIST.user_search_get, { "type": type, "page": page }),
+
+    // 获取详情页列表
+    article: id => $.get(APILIST.user_article_get, { id })
 }
 
 
 
 
-
-/*  url: APILIST.user_log_get,
-            type: "post",
-            data: fd,
-            processData: false, // 不允许处理数据
-            contentType: false, // 不要设置请求头 */
